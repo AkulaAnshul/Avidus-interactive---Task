f26d58b1c1bf056ff9fc69fc86bfe089c2d7a11f
AkulaAnshul <akulaanshulrao@gmail.com>
2026-05-28 11:43:13 +0530

    feat: [Minor Task 2.3] - Implement Task Schema with owner ref

---
+++ backend/models/Task.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please add a task title'],
+    trim: true,
+    maxlength: [100, 'Title cannot be more than 100 characters']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a task description'],
+    trim: true
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Completed'],
+    default: 'Pending'
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
